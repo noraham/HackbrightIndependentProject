458fd82126a435d0acacac2f02a213790aea51cf
Nora Hammack <noraham@gmail.com>
2018-02-06 18:00:04 -0800

    Made routes and template shells, not complete yet

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from tablesetup import connect_to_db, db
+from tablesetup import User, Foodstuff, Location, Barcode, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -19,11 +19,54 @@ app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
-def index():
-    """Homepage."""
+def Log_in_form_display():
+    """Homepage, log in"""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
 
+@app.route('/register')
+def newuser_form_display():
+    """Register page"""
+
+    return render_template("register.html")
+
+@app.route('/add')
+def foodstuff_form_display():
+    """Display add form"""
+
+    return render_template("add.html")
+
+@app.route('/pantry')
+def pantry_display():
+    """Display pantry from database"""
+
+    # query foodstuffs by user_id and location_id, pass to template
+
+    return render_template("pantry.html")
+
+@app.route('/edit')
+def edit_item():
+    """Display edit form"""
+
+    # need to add variable to route address
+
+    return render_template("edit.html")
+
+@app.route('/shop')
+def store_form_display():
+    """Display shopping list form"""
+
+    # query foodstuffs by is_shopping=True, pass to template
+
+    return render_template("store.html")
+
+@app.route('/eatme')
+def eatme_display():
+    """Display eatme"""
+
+    # query foodstuffs for items with exp, order by ascending, pass to template.
+
+    return render_template("eatme.html")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
