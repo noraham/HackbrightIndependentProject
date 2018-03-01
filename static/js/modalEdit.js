8e21a59b10bc383337405019ab9d7c4f8943cba7
Nora Hammack <noraham@gmail.com>
2018-03-01 01:26:08 -0800

    started converting item edit page to modal JS form

--- static/js/modalEdit.js
+++ static/js/modalEdit.js
@@ -1,7 +1,9 @@
 "use strict";
 
 $("#locform").hide();
+// $("#foodform").hide();
 
+// Listener and function to display LOCATION form in modal
 function locationEditDisplay(evt) {
     evt.preventDefault();
 
@@ -16,10 +18,10 @@ function locationEditDisplay(evt) {
 $(".locEventListener").on('click', locationEditDisplay);
 
 
+// listener and functions to handle LOCATION form submission - update db and change view
 function updatePantryPage(result) {
     alert(result);
 }
-
 
 function locationUpdate(evt) {
     evt.preventDefault();
@@ -34,10 +36,52 @@ function locationUpdate(evt) {
 
     $.post("/updatelocationformhandle", formInput, updatePantryPage)
     $(".locEventListener").html(new_name);
+    // move this to upper function?
 }
 
 $(".locSubmit").on('click', locationUpdate);
 
+
+// need to clear modal between clicks, NOT WORKING
 $('locEditModal').on('hidden.bs.modal', function () {
     $(this).find('.locid').trigger('reset');
 })
+
+
+// Listener and function to display FOOD form in modal
+function foodEditDisplay(evt) {
+    console.log("Hi");
+    let itemId = $(this).data("itemid");
+    let itemName = $(this).data("itemname");
+    console.log(itemId);
+    console.log(itemName);
+
+    // $("#foodform").show();
+}
+
+$("#foodEditModal").on('click', foodEditDisplay);
+
+// listener and functions to handle FOOD form submission - update db and change view
+function updatePantryPageFood(result) {
+    alert(result);
+}
+
+function foodUpdate(evt) {
+    evt.preventDefault();
+
+    // // get all fields from form, put in formInput
+    // let locId = $(this).data("locid");
+    // let new_name = $("#new_name").val()
+    // let formInput = {
+    //     "new_name": new_name,
+    //     "loc_id": locId
+    // }
+    // console.log(formInput)
+
+    // $.post("/edit/<int:pantry_id>", formInput, updatePantryPageFood)
+    // // change name and whether is still on page
+    // $(".locEventListener").html(new_name);
+    // // move this to upper function?
+}
+
+$(".foodSubmit").on('click', foodUpdate);
